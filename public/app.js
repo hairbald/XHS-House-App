e834d5f6100a435f33c86b91222b3659d17a6205
Harry <106716892+hairbald@users.noreply.github.com>
2023-03-10 14:55:41 -0600

    yo yo it werk!

--- public/app.js
+++ public/app.js
@@ -28,3 +28,18 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello, ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        //not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
